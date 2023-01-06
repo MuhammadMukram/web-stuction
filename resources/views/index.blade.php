d4ddfbd2c68de35823f5411c757cc8d10ea801bb
Sofyan Pujas <92896782+sofyanox12@users.noreply.github.com>
2023-01-07 00:11:00 +0800

    making the navbar responsive

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,19 +23,19 @@
 <body>
 
     <nav class="navbar navbar-light navbar-expand-lg bg-grey justify-content-center">
-        <div class="container mx-5">
-            <a href="/" class="navbar-brand d-flex w-50 mr-auto">
+        <div class="container-fluid mx-5">
+            <a href="/" class="navbar-brand">
                 <span class="text-orange">Stuc</span><span class="text-black">tion</span>
             </a>
 
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#stuction-navbar">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-element">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="navbar-collapse collapse w-100" id="stuction-navbar">
+            <div class="navbar-collapse collapse w-100 ms-auto" id="navbar-element">
 
                 <ul class="navbar-nav w-100 justify-content-center">
-                    <li class="nav-item">
+                    <li class="nav-item my-2">
                         <div class="form-group has-search">
                             <span class="bi bi-search text-orange d-inline-block search-icon ps-2"></span>
                             <span class="search-icon text-grey ps-5">|</span>
@@ -46,19 +46,19 @@
 
                 <ul class="nav navbar-nav ml-auto w-100 d-flex justify-content-end align-middle">
 
-                    <li class="nav-item me-5">
+                    <li class="nav-item me-4 my-2">
                         <button class="btn bg-orange">
                             <i class="bi bi-plus-circle-fill text-white"></i>
                             <span class="ms-2 text-white">New Post<span>
                         </button>
                     </li>
 
-                    <li class="nav-item profile d-flex align-middle">
+                    <li class="nav-item my-2 profile d-flex align-middle">
                         <div>
                             <i class="bi bi-person-circle"></i>
                             <span class="text-orange">Jones Doe<span>
                         </div>
-                        <div class="nav-item dropdown">
+                        <div class="dropdown">
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                                 aria-expanded="false">
                             </a>
@@ -78,10 +78,7 @@
             </div>
         </div>
     </nav>
-
-
-
-
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     
     
