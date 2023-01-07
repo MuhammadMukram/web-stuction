27cee376c11b4f16b5eb386f751077b81094b64b
Sofyan Pujas <92896782+sofyanox12@users.noreply.github.com>
2023-01-08 00:03:25 +0800

    formatting

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
+    {{-- Meta --}}
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -10,18 +11,18 @@
     <title>Stuction</title>
 
     {{-- Styles --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('styles/index.css') }}">
 
+    {{-- Scripts --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.js"></script>
 
+    {{-- Page's Styles --}}
     @yield('styles')
 
 </head>
 
 <body>
-
     <nav class="navbar navbar-light navbar-expand-md bg-grey justify-content-center">
         <div class="container-fluid mx-5">
             <a href="/" class="navbar-brand">
@@ -89,7 +90,7 @@
         integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     </script>
 
-
+    {{-- Page's Scripts  --}}
     @yield('scripts')
 </body>
 
