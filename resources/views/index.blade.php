5dc6b49efe132eaacc7e781437f62a5d5745d05a
Sofyan Pujas <92896782+sofyanox12@users.noreply.github.com>
2023-01-07 23:52:45 +0800

    fix navbar responsive & put them to `navbar.css`

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -33,7 +33,6 @@
             </button>
 
             <div class="navbar-collapse collapse w-100 ms-auto" id="navbar-element">
-
                 <ul class="navbar-nav w-100 justify-content-center">
                     <li class="nav-item d-flex my-2">
                         <div class="form-group has-search">
@@ -46,21 +45,25 @@
 
                 <ul class="nav navbar-nav ml-auto w-100 d-flex justify-content-end align-middle column-gap-5">
                     <div>
-                        <li class="nav-item d-flex my-2">
+                        <li class="nav-item new-post-button d-flex my-2">
                             <button class="btn bg-orange">
                                 <i class="bi bi-plus-circle-fill text-white"></i>
                                 <span class="ms-2 text-white">New Post<span>
                             </button>
                         </li>
                     </div>
-                    <div>
+
+                    <div class="profile-wrapper">
                         <li class="nav-item my-2 profile d-flex align-middle">
-                            <div>
-                                <i class="bi bi-person-circle"></i>
-                                <span class="text-orange">Jones Doe<span>
+                            <div class="d-flex column-gap-3">
+                                <button class="bg-black btn rounded-circle">
+                                    <i class="bi bi-person text-orange"></i>
+                                </button>
+                                <span class="text-orange my-auto">Jones Doe<span>
                             </div>
+
                             <div class="dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button"
+                                <a class="nav-link dropdown-toggle ms-1" href="#" role="button"
                                     data-bs-toggle="dropdown" aria-expanded="false">
                                 </a>
 
@@ -81,99 +84,7 @@
         </div>
     </nav>
 
-    <!-- Navbar -->
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-        <!-- Container wrapper -->
-        <div class="container">
-            <!-- Navbar brand -->
-            <a class="navbar-brand" href="#"><img id="MDB-logo"
-                    src="https://mdbcdn.b-cdn.net/wp-content/uploads/2018/06/logo-mdb-jquery-small.png" alt="MDB Logo"
-                    draggable="false" height="30" /></a>
-
-            <!-- Toggle button -->
-            <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
-                data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <i class="fas fa-bars"></i>
-            </button>
-
-            <!-- Collapsible wrapper -->
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left links -->
-                <ul class="navbar-nav me-3">
-                    <li class="nav-item">
-                        <a class="nav-link active d-flex align-items-center" aria-current="page" href="#"><i
-                                class="fas fa-bars pe-2"></i>Menu</a>
-                    </li>
-                </ul>
-                <!-- Left links -->
-
-                <form class="d-flex align-items-center w-100 form-search">
-                    <div class="input-group">
-                        <button class="btn btn-light dropdown-toggle shadow-0" type="button" data-mdb-toggle="dropdown"
-                            aria-expanded="false" style="padding-bottom: 0.4rem;">
-                            All
-                        </button>
-                        <ul class="dropdown-menu dropdown-menu-dark fa-ul">
-                            <li>
-                                <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
-                                            class="fas fa-search"></i></span>All</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
-                                            class="fas fa-film"></i></span>Titles</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
-                                            class="fas fa-tv"></i></span>TV
-                                    Episodes</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
-                                            class="fas fa-user-friends"></i></span>Celebs</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
-                                            class="fas fa-building"></i></span>Companies</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
-                                            class="fas fa-key"></i></span>Keywords</a>
-                            </li>
-                            <li>
-                                <hr class="dropdown-divider" />
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
-                                            class="fas fa-search-plus"></i></span>Advanced
-                                    search<i class="fas fa-chevron-right ps-2"></i></a>
-                            </li>
-                        </ul>
-                        <input type="search" class="form-control" placeholder="Search" aria-label="Search" />
-                    </div>
-                    <a href="#!" class="text-white"><i class="fas fa-search ps-3"></i></a>
-                </form>
-
-                <ul class="navbar-nav ms-3">
-                    <li class="nav-item me-3">
-                        <a class="nav-link d-flex align-items-center" href="#!">IMDbPro</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link d-flex align-items-center me-3" href="#!">
-                            <i class="fas fa-bookmark pe-2"></i> Watchlist
-                        </a>
-                    </li>
-                    <li class="nav-item" style="width: 65px;">
-                        <a class="nav-link d-flex align-items-center" href="#!">Sign In</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- Collapsible wrapper -->
-        </div>
-        <!-- Container wrapper -->
-    </nav>
-    <!-- Navbar -->
-
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     </script>
