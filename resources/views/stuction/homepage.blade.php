a67e6df4192aa370b91f01b0174ece4c5eb748e2
Sofyan Pujas <92896782+sofyanox12@users.noreply.github.com>
2023-01-09 09:15:41 +0800

    added `content` and its functionalities

--- resources/views/stuction/homepage.blade.php
+++ resources/views/stuction/homepage.blade.php
@@ -14,18 +14,23 @@
 @section('content')
     <div class="container my-5 pt-5 justify-content-center">
         <div class="content w-100">
-
             <div class="card">
-                <img src="{{ asset('sample/card.jpg') }}" class="card-img-top" alt="image">
-                <div class="mask text-light d-flex justify-content-center flex-column text-center position-absolute top-0 w-100 rounded-top" style="background-color: rgba(0, 0, 0, 0.5)">
-                    <div class="container">
-                        <button class="btn bg-orange text-white rounded-pill">View</button>
-                    </div>
-                </div>
+                <img src="{{ asset('sample/card.jpg') }}" class="card-img-top position-absolute">
+                <figure class="imghvr-fade">
+                    {{-- NOTE : Nanti cari cara lain supaya gambarnya cuma perlu di load 1 saja --}}
+                    <img src="{{ asset('sample/card.jpg') }}" class="overlay">
+                    <figcaption>
+                        <div class="container-fluid h-100 d-flex justify-content-center">
+                            <button class="btn bg-orange text-white rounded-pill my-auto px-4">View</button>
+
+                        </div>
+                    </figcaption>
+                </figure>
+
                 <div class="card-body d-flex">
                     <div class="col d-flex flex-column my-auto">
                         <span>
-                            <a href="#" class="title ">lorem20</a>
+                            <a href="#" class="title">lorem20</a>
                         </span>
                         <span class="date">DD/MM/YYYY</span>
                     </div>
@@ -37,26 +42,63 @@
                             </a>
                         </div>
 
-                        <div class="d-flex comments align-items-center gap-1">
-                            <i class="bi bi-chat"></i>
-                            <span class="ms-1">0</span>
+                        <div class="d-flex comment align-items-center gap-1">
+                            <button class="bg-transparent border-0 p-0 comment-button">
+                                <i class="bi bi-chat"></i>
+                            </button>
+                            <span class="ms-1">30012003</span>
                         </div>
 
                         <div class="d-flex like align-items-center gap-1">
-                            <i class="bi bi-heart"></i>
-                            <span class="ms-1">0</span>
+                            <button class="bg-transparent border-0 p-0 like-button">
+                                <i class="bi bi-heart"></i>
+                            </button>
+                            <span class="ms-1">120603</span>
                         </div>
                     </div>
                 </div>
             </div>
 
-            
+
         </div>
     </div>
+
+
 @endsection
 
 
 @section('scripts')
     <script src="{{ asset('scripts/user-navbar.js') }}"></script>
-    <script></script>
+
+    <script>
+        const likeButtons = document.querySelectorAll('.like-button');
+        const likeCounters = document.querySelectorAll('.like span');
+        const commentButtons = document.querySelectorAll('.comment-button');
+        const commentCounters = document.querySelectorAll('.comment span');
+
+        function countFormat(num) {
+            return new Intl.NumberFormat('en-GB', { notation: "compact", compactDisplay: "short" }).format(num)
+        }
+
+        likeButtons.forEach(button => {
+            button.addEventListener('click', () => {
+
+                if (!button.classList.contains('liked')) {
+                    button.classList.toggle('liked');
+                    button.innerHTML = '<i class="bi bi-heart-fill text-danger"></i>';
+                } else { 
+                    button.classList.toggle('liked');
+                    button.innerHTML = '<i class="bi bi-heart"></i>';
+                }
+                
+
+
+            });
+        });
+
+        likeCounters.forEach(total => {total.innerHTML = countFormat(parseInt(total.innerHTML))});
+        commentCounters.forEach(total => {total.innerHTML = countFormat(parseInt(total.innerHTML))});
+
+    </script>
+
 @endsection
